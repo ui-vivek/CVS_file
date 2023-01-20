79ec6f951a43672d31bf1e6156adcb5a3ba6d008
vivek singh <cu.vivek.1557@gmail.com>
2023-01-20 11:50:26 +0530

    changing localhost to 127

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 /*******************MAKING CONNECTION***************************/
-mongoose.connect("mongodb://localhost/CSV");
+mongoose.connect("mongodb://127.0.0.1/CSV");
 //setting it to db
 const db = mongoose.connection;
 

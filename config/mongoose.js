d734a5a49758319cc93e8d63b95a0949e0e21335
vivek singh <cu.vivek.1557@gmail.com>
2023-01-20 12:17:51 +0530

    127 to localhost

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(chalk.red.inverse(err)));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1/CSV');
+  await mongoose.connect('mongodb://localhost/CSV');
   console.log(chalk.green.inverse("Successfully connected to DataBase."))
 }
 const db=mongoose.connect;

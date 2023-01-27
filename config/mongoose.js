93efe36bf0d941d27c48816b4aaf4bcbd0f5ec57
vivek singh <55220203+ui-vivek@users.noreply.github.com>
2023-01-28 00:37:03 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(chalk.red.inverse(err)));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://csvfilecheckernodejs-user:csvfilecheckernodejs-user@cluster0.lzeuen4.mongodb.net/cccccc?retryWrites=true&w=majority');
+  await mongoose.connect('mongodb://127.0.0.1/test');
   console.log(chalk.green.inverse("Successfully connected to DataBase."))
 }
 const db=mongoose.connect;
